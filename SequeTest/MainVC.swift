62f9f22082b6f2c18672437fc0305d6f1afd0067
amandademetrio <amandademetrio@gmail.com>
2018-09-10 22:48:34 -0700

    first commit

--- SequeTest/MainVC.swift
+++ SequeTest/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  SequeTest
 //
 //  Created by Amanda Demetrio on 9/10/18.
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainVC: UIViewController {
+    
+    
+    @IBAction func performeSegueButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "FirstToSecondSegue", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
